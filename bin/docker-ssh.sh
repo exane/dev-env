065c74631518293fa8bb3fb3603e33b2b939decd
Tim Meier <raco0n@gmx.de>
2017-11-02 13:15:08 +0100

    Add port forwarding shortcut

--- bin/docker-ssh.sh
+++ bin/docker-ssh.sh
@@ -7,6 +7,13 @@ shift
 # To save the real arguments
 arguments=""
 
+# expands port arguments
+# docker ssh -p 3000 -> docker ssh -p 3000:3000
+transform_port_forwarding() {
+  local port_option=$1
+  echo $port_option | sed "s/-p \([0-9]\+\)/-p \1:\1/g"
+}
+
 for arg in $*
 do
   case $arg in
@@ -14,6 +21,7 @@ do
       shift
       docker_options=$(echo $@ | sed "s#^\(.*\) --.*#\1#")
       docker_entrypoint=$(echo $@ | sed "s#.*-- \(.*\)\$#\1#")
+      docker_options=$(transform_port_forwarding "$docker_options")
       docker run \
         --add-host "dev.docker:192.168.99.100" \
         -v $(PWD):/work \
