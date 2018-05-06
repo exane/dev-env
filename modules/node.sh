145011b2064be5c6cd7d18f358ff12b4b22cfbd4
Tim Meier <raco0n@gmx.de>
2018-05-06 14:06:26 +0200

    Make yarn global install persistant

--- modules/node.sh
+++ modules/node.sh
@@ -1,9 +1,7 @@
 set -eu
 
+# nvm install
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-# echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.zshrc
-# echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> $HOME/.zshrc
-# echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> $HOME/.zshrc
 
 nvm=$(cat <<-EOF
   if [ ! -d /store/nvm ]; then
@@ -18,8 +16,21 @@ EOF
 )
 echo $nvm >> ~/.zshrc
 
+# yarn install
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 sudo apt-get update -y && sudo apt-get install yarn -y
+
+yarn=$(cat <<-EOF
+  if [ ! -d /store/config ]; then
+    mkdir /store/config/;
+    cp -r ~/.config/* /store/config;
+  fi;
+  rm -rf ~/.config;
+  ln -s /store/config -T ~/.config;
+  export PATH=$PATH:~/.config/yarn/global/node_modules/.bin
+EOF
+)
+echo $yarn >> ~/.zshrc
